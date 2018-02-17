afdadaaef18b61c2097aa52ab0acf323f8ec9d3d
holdercp <holdercp@icloud.com>
2018-02-17 16:55:16 -0500

    Refactor playSound event listener into own function

--- day-1-drumkit/js/main.js
+++ day-1-drumkit/js/main.js
@@ -1,13 +1,13 @@
-window.addEventListener('keydown', (e) => {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-
+  
   if (audio) {
     audio.currentTime = 0;
     audio.play();
     key.classList.add('key--playing');
   }
-});
+}
 
 function removeClass(e) {
   if (e.propertyName === 'transform') {
@@ -17,3 +17,4 @@ function removeClass(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeClass));
+window.addEventListener('keydown', playSound);
