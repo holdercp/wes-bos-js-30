acefd26e2ce37a752986979abfa141a6755b59a4
holdercp <holdercp@icloud.com>
2018-02-17 16:49:20 -0500

    Complete MVP

---
+++ day-1-drumkit/js/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('keydown', (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (audio) {
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('key--playing');
+  }
+});
+
+function removeClass(e) {
+  if (e.propertyName === 'transform') {
+    this.classList.remove('key--playing');
+  }
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeClass));
