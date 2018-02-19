39709a0e6b598a295df2d90413f7a9fd98f0cfbd
holdercp <holdercp@icloud.com>
2018-02-18 20:47:48 -0500

    Fix transition jump when deg = 0

--- day-2-clock/js/main.js
+++ day-2-clock/js/main.js
@@ -1,20 +1,28 @@
 const hourHand = document.querySelector('.clock__hand--hour');
 const minHand = document.querySelector('.clock__hand--min');
 const secHand = document.querySelector('.clock__hand--sec');
+const allHands = document.querySelectorAll('.clock__hand');
 
 function rotateHands() {
   const now = new Date();
 
   const hour = now.getHours();
-  const hourDeg = ((hour / 12) * 360) + 90;
-  hourHand.style.transform = `rotate(${hourDeg}deg)`;
-
   const min = now.getMinutes();
-  const minDeg = ((min / 60) * 360) + 90;
-  minHand.style.transform = `rotate(${minDeg}deg)`;
-
   const sec = now.getSeconds();
-  const secDeg = ((sec / 60) * 360) + 90;
+
+  const hourDeg = ((hour / 12) * 360) + 90;
+  const minDeg = ((min / 60) * 360) + 90;
+  const secDeg = ((sec / 60) * 360) + 90; 
+
+  // Fixes sudden jump when secDeg is 0 (90)
+  if (secDeg === 90) {
+    allHands.forEach(hand => hand.style.transition = 'none');
+  } else {
+    allHands.forEach(hand => hand.style.transition = '');
+  }
+
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
+  minHand.style.transform = `rotate(${minDeg}deg)`;
   secHand.style.transform = `rotate(${secDeg}deg)`;
 }
 
